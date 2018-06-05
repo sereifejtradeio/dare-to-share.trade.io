1bed76913490836c4637a2e5a7d44191a7226a7b
dmarkitanis <dmarkitanis@gmail.com>
2018-06-05 10:48:03 +0300

    Gleam Fixes

--- js/main-fullpage.js
+++ js/main-fullpage.js
@@ -28,4 +28,15 @@ $(window).load(function() {
             "background-image": "url(" + $(this).attr("data-image") + ")"
         })
     })
+//    (function(d, t){
+//        if(window.location.hash!='#gleam'&&(''+document.cookie).match(/(^|;)\s*GleamTDvJ6=X($|;)/))
+//        {
+//            return;
+//        }
+//            var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+//            g.src = "https://js.gleam.io/TDvJ6/ol.js"; 
+//            s.parentNode.insertBefore(g, s);
+//    }(document, "script"));
 });
+
+
